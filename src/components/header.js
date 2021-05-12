2c16e86950dbb172bbd43416f2f8100a9d366144
wonho-jung <wonho.jung92@gmail.com>
2021-05-11 21:26:54 -0400

    use gatsby

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,60 @@
 import * as React from "react"
-import PropTypes from "prop-types"
 import { Link } from "gatsby"
-
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+import styled from "styled-components"
+import { FaBars } from "react-icons/fa"
+import { menuData } from "../data/MenuData"
+const Header = () => {
+  return (
+    <Nav>
+      <NavLink to="/">EXPLORIX</NavLink>
+      <Bars />
+      <NavMeun>
+        {menuData.map((item, index) => (
+          <NavLink key={index} to={item.link}>
+            {item.title}
+          </NavLink>
+        ))}
+      </NavMeun>
+    </Nav>
+  )
 }
 
 export default Header
+const Nav = styled.nav`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw- 1300px) / 2);
+  z-index: 100;
+  position: relative;
+`
+const NavLink = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+`
+const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`
+const NavMeun = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
